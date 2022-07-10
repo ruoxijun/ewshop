94e850883a2b6f667ed724ffa28eb93488c4daf5
ruoxijun <1460662245@qq.com>
2022-07-10 22:23:07 +0800

    添加注释

--- src/network/home.js
+++ src/network/home.js
@@ -7,12 +7,13 @@ export function getHomeAllData() {
   });
 }
 
+// 首页书籍列表
 export function getHomeGoods(type="sales", page=1) {
   return request({
     url: 'api/index',
     method: 'GET',
     params: {
-      ...JSON.parse(`{"${type}":1}`),
+      ...JSON.parse(`{"${type}":1}`), // 选项卡分类
       page,
     },
   });
